5b6f70ca3f81d1f7e28f981d0c15bf272a8a5739
pilagod <r03725029@ntu.edu.tw>
2015-08-14 18:12:52 +0800

    news refine

--- js/common/common.js
+++ js/common/common.js
@@ -1,6 +1,14 @@
 (function () {
   var $header = $('.header'),
-      $firstSection = $('.header ~ section.section').first();
+      $firstSection = $('.header ~ section.section').first(),
+      $newsDescription = $('.news .news-desc');
+
+  if ($newsDescription.length) {
+    $newsDescription.each(function () {
+      console.log($(this));
+      $(this).dotdotdot();
+    });
+  }
 
   if (!$firstSection.hasClass('slider')) {
     $firstSection.css('padding-top', parseInt($firstSection.css('padding-top')) + $header.height());
