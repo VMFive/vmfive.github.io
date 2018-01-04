0d4aaa955be16160db2c4349924304a543cd332f
John Lee <johnteee@gmail.com>
2018-01-04 17:57:44 +0800

    Add default index(redirections only)

--- jade/make.sh
+++ jade/make.sh
@@ -5,14 +5,14 @@ find $OUT -name '*.html' -exec rm {} +
 jade . -O lang/zh_tw.json
 mkdir $OUT/zh_tw
 cp -R ./* $OUT/zh_tw
-find . -name '*.html' -exec rm {} +
 
 jade . -O lang/en.json
 mkdir $OUT/en
 cp -R ./* $OUT/en
+
+# Leave an index
+cp ./index.html.default $OUT/index.html
 find . -name '*.html' -exec rm {} +
-
-
 
 cd $OUT
 find . -name '*.jade' -exec rm {} +
